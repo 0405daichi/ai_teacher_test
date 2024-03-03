95bb4e15ee2f9427c7c1373f659f601da935d841
0405daichi <daichi.fukunaga.45@gmail.com>
2024-03-03 16:07:14 +0900

    debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -124,7 +124,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'ai-teacher-test.onrender.com',
+    domain:               'gmail.com',
     user_name: ENV["GMAIL_USERNAME"] ,
     password: ENV["GMAIL_PASSWORD"] ,
     authentication:       'plain',
