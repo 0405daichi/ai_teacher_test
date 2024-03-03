af4d619e7e835b3f62fd92e42a72783d8c5be554
0405daichi <daichi.fukunaga.45@gmail.com>
2024-03-03 15:31:48 +0900

    debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,4 +120,14 @@ Rails.application.configure do
 
   # メールで使用するURLのホストを設定
   config.action_mailer.default_url_options = { host: 'ai-teacher-test.onrender.com', protocol: 'https' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'ai-teacher-test.onrender.com',
+    user_name: ENV["GMAIL_USERNAME"] ,
+    password: ENV["GMAIL_PASSWORD"] ,
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
